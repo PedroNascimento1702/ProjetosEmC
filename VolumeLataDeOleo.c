89420d18a5f6ed6f972e2cc5f8de70dc81c20603
Pedro Nascimento <pedronascimento1702@gmail.com>
2020-09-02 19:33:01 -0300

    Exercicios

--- VolumeLataDeOleo.c
+++ VolumeLataDeOleo.c
@@ -8,9 +8,11 @@ int main()
 	float Volume = 0;
 	printf("================== PROGRAMA VOLUME DE UMA LATA DE OLEO ==================");
 	printf("\n\n Insira o valor do RAIO da lata de oleo\n ");
+	fflush(stdin);
 	scanf("%i",&Raio);
 
 	printf("\n\n Insira o valor da ALTURA da lata de oleo\n ");
+	fflush(stdin);
 	scanf("%i",&Altura);
 	
 	Volume = 3.14159 * (Raio * Raio) * Altura;
